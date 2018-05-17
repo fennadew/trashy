80b7df382bf9444e08f1597c6569f6f0406390b7
Fenna <fennadewilde@hotmail.com>
2018-05-17 13:18:30 +0200

    deploy

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render(<BrowserRouter basename="/trashy/"><App /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
